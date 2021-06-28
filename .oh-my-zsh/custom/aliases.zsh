1f82ddb720b46e617ea275ecea0a0e08e54e5b55
Ivan Penchev <ivan.penchev@maersk.com>
2021-06-28 12:42:19 +0200

    tf alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,3 +3,5 @@ alias git_prune="git fetch --prune && git branch -vv | grep 'origin/.*: gone]' |
 
 # Generate a secure password and copy it to clipboard
 alias genpw='LC_ALL=C tr -dc "[:alnum:]" < /dev/urandom | head -c 20 | pbcopy'
+
+alias tf='terraform'
