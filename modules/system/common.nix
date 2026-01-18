9611734666eba251b9ab0e2c29e3ea3a01f596ee
Ivan Penchev <ivan@penchev.com>
2026-01-18 09:20:36 +0100

    chore: added gc to nix

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -9,7 +9,33 @@ with lib;
     # Allow unfree packages globally
     nixpkgs.config.allowUnfree = true;
 
-    # Enable flakes
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    # Nix configuration
+    nix = {
+      settings = {
+        # Enable flakes
+        experimental-features = [ "nix-command" "flakes" ];
+        # Optimise storage by hardlinking identical files
+        auto-optimise-store = true;
+        # Allow the user to manage nix
+        trusted-users = [ "root" config.ivan-config-options.user.name ];
+      };
+
+      # Garbage Collection
+      # Automatically remove old build dependencies and unused packages
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 7d";
+      } // (if pkgs.stdenv.isDarwin then {
+        # macOS specific interval
+        interval = {
+          Hour = 1;
+          Minute = 0;
+          Weekday = 7;
+        };
+      } else {
+        # Linux (NixOS) specific interval
+        dates = "weekly";
+      });
+    };
   };
 }
